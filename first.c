ef73882ec284aafa71d596febaa356d72bf043ff
rishab11250 <rishab.chandgothia.cg@gmail.com>
2025-08-13 11:45:21 +0530

    Lower and upper

--- first.c
+++ first.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    printf("hi");
+    char a;
+    scanf("%c",&a);
+    if(a >= 'A' && a <= 'Z'){
+        printf("Upper case");
+    }
+    else if(a>='a'&&a<='z'){
+        printf("Lower case");
+    }
+    else{
+        printf("Not a alphabet");
+    }
     return 0;
 }
